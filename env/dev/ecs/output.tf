412068975988d763474322acb64fefc6d192a2dd
Kaji <kajihiro@88665a11c164.ant.amazon.com>
2022-10-23 15:32:59 +0900

    add alb output

--- env/dev/ecs/output.tf
+++ env/dev/ecs/output.tf
@@ -5,3 +5,15 @@ output "ecs_cluster_name" {
 output "ecs_service_ec2_name" {
   value = module.ecs.service_ec2_name
 }
+
+output "alb_listner_arn" {
+  value = module.ecs.listner_arn
+}
+
+output "alb_target_group_blue_name" {
+  value = module.ecs.target_group_blue_name
+}
+
+output "alb_target_group_green_name" {
+  value = module.ecs.target_group_green_name
+}
--- modules/ecs/output.tf
+++ modules/ecs/output.tf
@@ -5,3 +5,15 @@ output "cluster_name" {
 output "service_ec2_name" {
   value = aws_ecs_service.frontend_ec2.name
 }
+
+output "listner_arn" {
+  value = aws_lb_listener.public.arn
+}
+
+output "target_group_blue_name" {
+  value = aws_lb_target_group.public-blue.name
+}
+
+output "target_group_green_name" {
+  value = aws_lb_target_group.public-green.name
+}
