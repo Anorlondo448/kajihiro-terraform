a20be7ef26337cf6e2010192aac204ee1df4e0c8
Kaji <kajihiro@88665a11c164.ant.amazon.com>
2022-10-23 15:06:34 +0900

    add ecs service

---
+++ modules/ecs/iam_ecs.tf
@@ -0,0 +1,50 @@
+###
+#
+# IAM for ECS task
+#
+# Task Execution Role
+resource "aws_iam_role" "task_execution" {
+  name  = "${var.system}-${var.env}-ecs-task-execution"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "ecs-tasks.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "execution" {
+  role       = aws_iam_role.task_execution.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+# Task Role
+resource "aws_iam_role" "task" {
+  name  = "${var.system}-${var.env}-ecs-task"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "ecs-tasks.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
---
+++ modules/ecs/service_ec2.tf
@@ -0,0 +1,42 @@
+###
+#
+# Service
+#
+resource "aws_ecs_service" "frontend_ec2" {
+  name             = "${var.system}-${var.env}-frontend"
+  cluster          = aws_ecs_cluster.main.id
+  task_definition  = aws_ecs_task_definition.frontend.arn
+  desired_count    = 1
+  # platform_version = "1.4.0"
+
+  capacity_provider_strategy {
+    capacity_provider = aws_ecs_capacity_provider.ecs_capacity_provider.name
+    weight            = 100
+  }
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.public-blue.arn
+    container_name   = "${var.system}-${var.env}-frontend"
+    container_port   = 80
+  }
+
+  network_configuration {
+    subnets = var.subnet_public_id
+
+    security_groups = [
+      var.security_group_id_allow_http_from_any,
+    ]
+  }
+
+  depends_on = [
+    aws_lb.public,
+  ]
+
+  lifecycle {
+    ignore_changes = [
+      desired_count,
+      task_definition,
+      capacity_provider_strategy
+    ]
+  }
+}
