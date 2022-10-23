c47e58470cebe2773eeffed4041f1cd888256a3f
Kaji <kajihiro@88665a11c164.ant.amazon.com>
2022-10-23 15:27:51 +0900

    add ecs output

--- env/dev/ecs/output.tf
+++ env/dev/ecs/output.tf
@@ -1,3 +1,7 @@
 output "ecs_cluster_name" {
   value = module.ecs.cluster_name
 }
+
+output "ecs_service_ec2_name" {
+  value = module.ecs.service_ec2_name
+}
--- modules/ecs/output.tf
+++ modules/ecs/output.tf
@@ -1,3 +1,7 @@
 output "cluster_name" {
   value = aws_ecs_cluster.main.name
 }
+
+output "service_ec2_name" {
+  value = aws_ecs_service.frontend_ec2.name
+}
