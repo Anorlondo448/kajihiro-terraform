612b57a66c9ec693f6f9711b92e46da5232ebefa
Kaji <kajihiro@88665a11c164.ant.amazon.com>
2022-10-31 13:41:18 +0900

    add fargate capacity provider

--- modules/ecs/cluster.tf
+++ modules/ecs/cluster.tf
@@ -6,11 +6,15 @@ resource "aws_ecs_cluster" "main" {
   name = "${var.system}-${var.env}-cluster"
 }
 
-# ASG
+# Capacity Provider
 resource "aws_ecs_cluster_capacity_providers" "asg" {
   cluster_name = aws_ecs_cluster.main.name
 
-  capacity_providers = [aws_ecs_capacity_provider.ecs_capacity_provider.name]
+  capacity_providers = [
+    "FARGATE",
+    "FARGATE_SPOT",
+    aws_ecs_capacity_provider.ecs_capacity_provider.name
+  ]
 
   default_capacity_provider_strategy {
     base              = 1
@@ -18,22 +22,3 @@ resource "aws_ecs_cluster_capacity_providers" "asg" {
     capacity_provider = aws_ecs_capacity_provider.ecs_capacity_provider.name
   }
 }
-
-# # Fargate
-# resource "aws_ecs_cluster_capacity_providers" "fargate" {
-#   cluster_name = aws_ecs_cluster.main.name
-
-#   capacity_providers = [
-#     "FARGATE",
-#     "FARGATE_SPOT"
-#   ]
-
-#   default_capacity_provider_strategy {
-#     capacity_provider = "FARGATE"
-#     weight            = 90
-#   }
-#   default_capacity_provider_strategy {
-#     capacity_provider = "FARGATE_SPOT"
-#     weight            = 10
-#   }
-# }
